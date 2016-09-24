1ade7803c505427b073f371b9f1a1ef42aac632f
JOCompute <joel.w.ottosson@gmail.com>
2016-09-24 12:14:25 +0200

    LessonsByDerekBanas is added to repository: lesson

---
+++ LessonsDerekBanas/src/lessonb17_Threads/GetTimeThread.java
@@ -0,0 +1,38 @@
+package lessonb17_Threads;
+
+import java.util.*; //Get date and locale
+import java.text.DateFormat; 
+
+
+
+public class GetTimeThread extends Thread{
+	public void run(){
+		
+		Date rightNow;
+		Locale currentLocale;
+		DateFormat timeFormat;
+		DateFormat dateFormat;
+		String timeOutput;
+		String dateOutput;
+		
+		for (int i = 0; i < 20; i++) {
+			rightNow = new Date();
+			currentLocale = new Locale("DE");
+			
+			timeFormat = DateFormat.getTimeInstance(DateFormat.DEFAULT, currentLocale);
+			dateFormat = DateFormat.getDateInstance(DateFormat.DEFAULT, currentLocale);
+			
+			timeOutput = timeFormat.format(rightNow);
+			dateOutput = dateFormat.format(rightNow);
+			
+			System.out.println(timeOutput);
+			System.out.println(dateOutput);
+			System.out.println();
+			
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+}
